9c9500a82e67845500c6d7de192ce22559b012d0
bridgitjung <bridgit@umich.edu>
2023-04-06 21:38:38 -0400

    make_players_table done

--- HW7.py
+++ HW7.py
@@ -53,7 +53,17 @@ def make_positions_table(data, cur, conn):
 #     created for you -- see make_positions_table above for details.
 
 def make_players_table(data, cur, conn):
-    pass
+    cur.execute("CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY, name TEXT, position_id INTEGER, birthyear INTEGER, nationality TEXT)")
+    for player in data['squad']:
+        name = player['name']
+        position = player['position']
+        birthyear = player['dateOfBirth'].split('-')[0]
+        nationality = player['nationality']
+        cur.execute("SELECT id FROM Positions WHERE position=?", (position,))
+        position_id = cur.fetchone()[0]
+        cur.execute("INSERT OR IGNORE INTO Players (id, name, position_id, birthyear, nationality) VALUES (?,?,?,?,?)",(player['id'], name, position_id, birthyear, nationality))
+    conn.commit()
+    # pass
 
 ## [TASK 2]: 10 points
 # Finish the function nationality_search
@@ -105,7 +115,7 @@ def birthyear_nationality_search(age, country, cur, conn):
     # HINT: You'll have to use JOIN for this task.
 
 def position_birth_search(position, age, cur, conn):
-       pass
+    pass
 
 
 # [EXTRA CREDIT]
